54df389d586eda0cdb45ff23394b251d7003af66
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-11-08 00:19:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Abdullah Arif, Clyde Rempillo, Andrew Myshok
-# COMP-4800 - Selected Topic in Software engineering project
+
 # Keystroke dynamics spoofer - a program created to spoof the keystrokes of users
 # created to test the strength of various keystroke dynamic based authentication systems
 
